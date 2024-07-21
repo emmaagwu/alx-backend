e76cd314363d21032da08e1df3032b1002bd4f9c
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-21 17:54:45 +0100

    Add pagination utility function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Pagination utility function.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Calculates the start and end indices for a given page and page size.
+
+    Args:
+        page (int): The current page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
