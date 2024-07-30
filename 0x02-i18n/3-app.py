bd5379e0e9c92cdd6b4b7fdc50a40ef0bb88b6c2
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-30 16:40:44 +0100

    Add Flask app with internationalization support

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This script sets up a basic Flask app with a single route that
+renders an index page, configures Babel for internationalization, and
+selects the best language based on request headers.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Config class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Renders the index page with a welcome message.
+
+    :return: Rendered HTML template for the index page.
+    """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determines the best match with the supported languages based on the
+    request's Accept-Language header.
+
+    :return: The best matched locale.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
